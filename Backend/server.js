228e0d2047ae588b3655d15bd5e1f7846c4a723b
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2025-10-29 09:37:09 +0530

    Established server with DB

---
+++ Backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./Config/db.js";
+// import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+// Routes
+// app.use("/api/auth", authRoutes);
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
